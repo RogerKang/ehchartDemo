7991174dab3ad93eefd4630994616e173255bc17
RogerKang <524180154@qq.com>
2017-04-15 14:09:42 +0800

    'addBusCache'

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 var express = require('express');
 var urllib = require('urllib');
-
+var busCache = null;
 var app = express();
 
 app.set('views', './views');
@@ -9,13 +9,19 @@ app.set('view engine', 'ejs')
 app.use('/static', express.static('static'));
 
 app.get('/linesBus', function (req, res, next) {
-    urllib.request('http://echarts.baidu.com/gallery/data/asset/data/lines-bus.json', function(err, data, response){
+    if(!busCache) {
+        urllib.request('http://echarts.baidu.com/gallery/data/asset/data/lines-bus.json', function (err, data, response) {
 
-        if(err)
-            res.json([]);
-        else
-            res.json(JSON.parse(data.toString()));
-    });
+            if (err)
+                res.json([]);
+            else {
+                res.json(JSON.parse(data.toString()));
+                busCache = data;
+            }
+        });
+    } else {
+        res.json(JSON.parse(busCache.toString()));
+    }
 });
 
 app.get('/pollution', function (req, res, next){
@@ -25,9 +31,22 @@ app.get('/pollution', function (req, res, next){
 
 });
 
+app.get('/migration', function (req, res, next){
+
+    urllib.request('http://echarts.baidu.com/echarts2/doc/example/data/migration.json', function(err, data, response){
+
+        if(err)
+            res.json([]);
+        else
+            res.json(JSON.parse(data.toString()));
+    });
+
+});
+
+
 app.use('/', function(req,res,next){
 
-    res.render('index2');
+    res.render('index');
 });
 
 
